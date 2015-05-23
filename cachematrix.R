27f92e2092d9f4e1261b2796ea34995ac25b2031
datamongo <thik93@gmail.com>
2015-05-23 14:50:34 -0700

    Added more comments and changed variable name to be more descriptive

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,45 @@
 ## makeCacheMatrix is a function that returns a vector which is a list
-## of functions. The list elements
+## of functions. The list elements are
 ## get - gets the value of the vector
 ## set - sets the value of the vector
 ## setinv - finds the value of the inverse of the matrix
 ## getinv - gets the value of the inverse of the matrix
 ## 
+## usage: a <- makeCacheMatrix(matrix(data,nrow,ncol,...))
+##        cacheSolve(a)
+## 
 ## cacheSolve solves the matrix and finds its inverse. If it is a new matrix,
 ## the inverse is calculated, otherwise if the inverse of the matrix entered
 ## has been calculated previosuly, the cached value of the matrix inverse 
-## is output
+## is output. The inverse is calculated using the solve function in R
 
 ## The functions set, get, setiv and getinv are defined inside of makeCacheMatrix
 ## The set and setinv functions set the return values to be global values
-## using the <<- operator. m is also initialized as a global variable. The
-## setinv function returns the inverse of the matrix and stores it as a
-## global variable m which can then be retrieved using getinv
+## using the <<- operator. matinv is also initialized as a global variable.
+## The setinv function returns the inverse of the matrix and stores it as a
+## global variable matinv which can then be retrieved using getinv
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        matinv <- NULL
+
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                matinv <<- NULL
         }
-        get <- function() x
-        setinv <- function(solve) m <<- solve
-        getinv <- function() m
+        
+        get <- function() {
+                x
+        }
+        
+        setinv <- function(solve) {
+                matinv <<- solve
+        }
+        
+        getinv <- function() {
+                matinv
+        }
+        
+## Return a list
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
@@ -32,22 +47,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## CacheSolve returns the inverse of a a matrix that is passed as an
-## argument to it. If the variable m is NULL then no matrix has been cached,
-## a new matrix is read into data and its inverse calculated, stored in
-## in variable m and returned. If the matrix inverse has already been
-## calculated then it is stored in m and the if loop is executed and the
-## cached value of m is returned
+## CacheSolve returns the inverse of a matrix that is passed as an
+## argument to it. If the variable matinv is NULL then no matrix has been
+## cached, a new matrix is read into data and its inverse calculated, stored
+## in variable matinv and returned. If the matrix inverse has already been
+## calculated then it has been stored in matinv, the if loop is executed and
+## cached value of matinv is returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        m <- x$getinv()
-        if(!is.null(m)) {
+        matinv <- x$getinv()
+        
+        if(!is.null(matinv)) {
                 message("Getting cached Matrix ... ")
-                return(m)
+                return(matinv)
         }
+        
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinv(m)
-        m
+        
+        matinv <- solve(data, ...)
+        
+        x$setinv(matinv)
+        
+        matinv
 }
